475aeb223e070e5f608afa5ae781998aff81cabe
LOL.2 <75392240+lolpointdeux@users.noreply.github.com>
2022-10-04 14:35:58 +0200

    Add files via upload

---
+++ I1P622_Rioli/I1P622_Rioli/Feux.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace I1P622_Rioli
+{
+    class Feux
+    {
+        private string id;
+        private string color;
+        private bool etat = false;
+
+        public Feux (string FeuxId, string FeuxColor)
+
+        {
+            id = FeuxId;
+            color = FeuxColor;
+
+        }
+        public void changeColor()
+        {
+            if (color == "rouge")
+            {
+                color = "vert";
+            }
+            color = "rouge";
+
+        } 
+        public void clignoter()
+        {
+          
+            string chain = "";
+
+            
+            chain = $"{id} est éteint";
+
+
+            Console.WriteLine(chain);
+
+            
+        }
+        public void chainString ()
+        {
+            string chain = "";
+
+            chain = $"Le feu  de signalisation {id} est {color} ";
+
+            Console.WriteLine(chain);
+
+        }
+
+
+
+    }
+}
+
+    
---
+++ I1P622_Rioli/I1P622_Rioli/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace I1P622_Rioli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string id;
+            string color;
+
+            Feux[] feux = new Feux[2];
+            feux[0] = new Feux("42", "rouge");
+            feux[1] = new Feux("12", "vert" );
+
+            Console.WriteLine("Etats des feux  :");
+            for (int i = 0; i < 2; i++)
+            {
+
+                feux[i].chainString();
+
+            }
+            Console.WriteLine("Changement d'Etat : ");
+            for (int i = 0; i < 5; i++)
+            {
+                feux[i].changeColor();
+                feux[i].chainString();
+            }
+            Console.WriteLine("Feu clignotant : "); 
+                for (int i = 0; i < 5; i++)
+                {
+                    feux[i].clignoter();
+
+                }
+               
+            }
+
+
+
+           /* Voulais randomizer les id des feux 
+            * for (int i = 0; i < 2; i++)
+            {            
+                Random random = new Random();
+                id = random.Next(1, 100).ToString();
+                int ran = random.Next(1, 2);
+                if (ran == 1)
+                {
+                    color = "rouge";
+                }
+                else
+                    color = "vert";
+
+                feux[i] = new Feux(id, color);
+            }*/
+            
+           
+            
+        }
+    }
+}
